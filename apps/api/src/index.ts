9999ea6422b052c0c8bf1b566b139ae159530fbb
Alvaro Sena <alvarosenacs.c@gmail.com>
2025-11-01 02:09:32 -0300

    chore: add app permissions

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,7 +1,5 @@
 import { defineAbilityFor } from "@saas/auth";
 
-const ability = defineAbilityFor({ role: "ADMIN" });
+const ability = defineAbilityFor({ role: "MEMBER", id: "user-id" });
 
-const userCanInviteSomeoneElse = ability.can("invite", "User");
-
-console.log(userCanInviteSomeoneElse);
+console.log(ability.can("get", "Billing"));
