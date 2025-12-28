2dc78fd726112a9372d8b0a39c9b093f74afa69c
Alvaro Sena <alvarosenacs.c@gmail.com>
2025-12-27 22:31:18 -0300

    feat: add sign in form and request the api with server actions

---
+++ apps/web/src/app/auth/sign-in/actions.tsx
@@ -0,0 +1,60 @@
+"use server";
+
+import { email, success, z } from "zod";
+
+import { signInWithPassword } from "@/http/sign-in-with-password";
+import { HTTPError } from "ky";
+
+const signInSchema = z.object({
+  email: z.email({ message: "Please, provide a valid email adress" }),
+  password: z.string().min(1, { message: "Please, provide your password" }),
+});
+
+export async function signInWithEmailAndPassword(data: FormData) {
+  const result = signInSchema.safeParse(Object.fromEntries(data));
+
+  if (!result.success) {
+    const errors = result.error.flatten().fieldErrors;
+
+    return {
+      success: false,
+      message: null,
+      errors,
+    };
+  }
+
+  const { email, password } = result.data;
+
+  try {
+    const { token } = await signInWithPassword({
+      email,
+      password,
+    });
+
+    console.log({ token });
+  } catch (err) {
+    if (err instanceof HTTPError) {
+      const { message } = await err.response.json();
+
+      return {
+        success: false,
+        message,
+        errors: null,
+      };
+    }
+
+    console.error(err);
+
+    return {
+      success: false,
+      message: "Unexpected error, try again in a few minutes.",
+      errors: null,
+    };
+  }
+
+  return {
+    success: true,
+    message: null,
+    errors: null,
+  };
+}
---
+++ apps/web/src/app/auth/sign-in/sign-in-form.tsx
@@ -0,0 +1,105 @@
+"use client";
+
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Separator } from "@/components/ui/separator";
+
+import { AlertTriangle, Github, Loader2 } from "lucide-react";
+import { signInWithEmailAndPassword } from "./actions";
+import { FormEvent, useState, useTransition } from "react";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+
+export function SignInForm() {
+  // const [{ success, message, errors }, formAction, isPending] = useActionState(
+  //   signInWithEmailAndPassword,
+  //   { success: false, message: null, errors: null }
+  // );
+
+  const [isPending, startTransition] = useTransition();
+
+  const [{ success, message, errors }, setFormState] = useState<{
+    success: boolean;
+    message: string | null;
+    errors: Record<string, string[]> | null;
+  }>({
+    success: false,
+    message: null,
+    errors: null,
+  });
+
+  async function handleSignIn(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const form = event.currentTarget;
+
+    const data = new FormData(form);
+
+    startTransition(async () => {
+      const state = await signInWithEmailAndPassword(data);
+
+      setFormState(state);
+    });
+  }
+
+  return (
+    <form onSubmit={handleSignIn} className="space-y-4">
+      {success === false && message && (
+        <Alert variant="destructive">
+          <AlertTriangle className="size-4" />
+          <AlertTitle>Sign in failed!</AlertTitle>
+          <AlertDescription>
+            <p>{message}</p>
+          </AlertDescription>
+        </Alert>
+      )}
+
+      <div className="space-y-1">
+        <Label htmlFor="email">Email</Label>
+        <Input name="email" type="email" id="email" />
+
+        {errors?.email && (
+          <p className="text-xs font-medium text-red-500 dark:text-red-400">
+            {errors.email[0]}
+          </p>
+        )}
+      </div>
+
+      <div className="space-y-1">
+        <Label htmlFor="password">Password</Label>
+        <Input name="password" type="password" id="password" />
+
+        {errors?.password && (
+          <p className="text-xs font-medium text-red-500 dark:text-red-400">
+            {errors.password[0]}
+          </p>
+        )}
+
+        <Link
+          href="/auth/forgot-password"
+          className="text-foreground text-xs font-medium hover:underline"
+        >
+          Forgot your password?
+        </Link>
+      </div>
+
+      <Button type="submit" className="w-full" disabled={isPending}>
+        {isPending && <Loader2 className="size-4 animate-spin" />}
+        Sign In with e-mail
+      </Button>
+
+      <Button type="button" variant="link" className="w-full" size="sm" asChild>
+        <Link href="/auth/sign-up">Create new account</Link>
+      </Button>
+
+      <Separator />
+
+      <Button type="submit" variant="outline" className="w-full">
+        <Github />
+        Sign In with Github
+      </Button>
+    </form>
+  );
+}
